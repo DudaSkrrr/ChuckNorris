1f99df131987ad89a77da913124d27d662a364ee
Cezar <marinacezar72@gmail.com>
2024-01-10 19:14:55 +0200

    small change

--- chucknorris/src/App.js
+++ chucknorris/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Categories from './components/categories'
 
 const App = () => {
   return (
     <div>
       <h1>Welcome to Chuck Norris jokes</h1>
+      <Categories/>
     </div>
   )
 }
